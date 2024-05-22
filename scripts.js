ebe363e466821035a565faf292eaa6a38a5c4b7f
kiran <kirandhakal715@gmail.com>
2024-05-22 15:22:16 +0545

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+      
+const observer =new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+    //   entry.target.classList.add('show');
+    entry.target.classList.add('show');
+
+    }else{
+    //   entry.target.classList.remove('show');
+    entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(
+  (el)=> observer.observe(el)
+);
